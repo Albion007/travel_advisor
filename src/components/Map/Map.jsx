da5a32bf04452584249594fc646c7a3fd300b3b1
Albion Morina <94832146+Albion007@users.noreply.github.com>
2023-02-18 23:57:00 +0100

    m

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
+
+import mapStyles from './mapStyles';
+
+const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked }) => {
+  const classes = useStyles();
+  const isDesktop = useMediaQuery('(min-width:600px)');
+
+
+  let gapi = window.gapi
+
+  const CLIENT_ID = "191560946240-uk7u7b6iceusprd60pq7khl8r5kq4buf.apps.googleusercontent.com";
+  const API_KEY = "AIzaSyDuwtC_l4ZMEaQPTSirUq5kbJJu9R_JT3o";
+  const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+  const SCOPES = "https://www.googleapis.com/auth/calendar.events"
+
+
+
+  const handleClick = () => {
+    gapi.load('client:auth2', () => {
+      console.log('loaded client')
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+
+      gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+
+      gapi.auth2.getAuthInstance().signIn()
+        .then(() => {
+
+          var event = {
+            'summary': 'Google I/O 2015',
+            'location': '800 Howard St., San Francisco, CA 94103',
+            'description': 'A chance to hear more about Google\'s developer products.',
+            'start': {
+              'dateTime': '2021-07-10T09:00:00-07:00',
+              'timeZone': 'America/Los_Angeles'
+            },
+            'end': {
+              'dateTime': '2021-07-10T17:00:00-07:30',
+              'timeZone': 'America/Los_Angeles'
+            },
+            'recurrence': [
+              'RRULE:FREQ=DAILY;COUNT=2'
+            ],
+            'attendees': [
+              {'email': 'lpage@example.com'},
+              {'email': 'sbrin@example.com'}
+            ],
+            'reminders': {
+              'useDefault': false,
+              'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10}
+              ]
+            }
+          };
+
+          var request = gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event,
+          })
+
+          request.execute(event => {
+            console.log(event)
+            window.open(event.htmlLink)
+          })
+
+          gapi.client.calendar.events.list({
+            'calendarId': 'primary',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime'
+          }).then(response => {
+            const events = response.result.items
+            console.log('EVENTS: ', events)
+          })
+
+        })
+    })
+  }
+
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDt8N4Xt8NiJvJY91RxzP5ufx2K9Xt4KtY' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {
+              !isDesktop ? (
+                <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              ) : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                    {place.name}
+                  </Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                    alt={place.name}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                  <button onClick={handleClick}>Add Event</button>
+                </Paper>
+              )
+            }
+          </div>
+        ))}
+      </GoogleMapReact>
+    </div>
+  );
+}
+
+export default Map;
